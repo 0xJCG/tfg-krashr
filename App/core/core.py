6d1bd9901b21dd7fccfbd9ed913b2c66fb5d0c1f
0xJCG <jonathancastrogonzalez@gmail.com>
2015-11-11 19:23:48 +0100

    Core almost done.

--- App/core/core.py
+++ App/core/core.py
@@ -13,13 +13,47 @@ __license__ = "Proprietary"
 __author__ = "Jonathan Castro"
 __author_email__ = "Jonathan Castro, jonathancastrogonzalez at gmail dot com"
 
-def is_valid_url(self):
-    import re
-    regex = re.compile(
-        r'^https?://'  # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?'  # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    return self.url is not None and regex.search(self.url)
+from .url import URL
+import json
+import os
 
+class Core:
+
+    def __init__(self, call):
+        self.__check_call(call)
+        self.url = ""
+        self.action = 0
+        self.modules = {}
+        self.results = {}
+
+    def __is_valid_url(self):
+        import re
+        regex = re.compile(
+            r'^https?://'  # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        return self.url is not None and regex.search(self.url)
+
+    def __check_modules(self):
+        # http://stackoverflow.com/questions/3207219/how-to-list-all-files-of-a-directory-in-python
+        path = os.getcwd()
+        for name in os.listdir(path + "/modules"):
+            if os.path.exists(os.path.join(path + name, "module.py")):
+                self.modules[name] = True
+            else:
+                self.modules[name] = False
+
+    def __check_call(self, call):
+        data = json.load(call)
+        self.url = data["url"]
+        self.action = data["action"]
+
+    def start(self):
+        if self.__is_valid_url():
+            for module, status in self.modules:
+                if status:
+                    # http://stackoverflow.com/questions/4230725/how-to-execute-a-python-script-file-with-an-argument-from-inside-another-python
+                    result = os.system(os.getcwd() + "/modules/" + module, URL(self.url))
+                    self.results[module] = result
