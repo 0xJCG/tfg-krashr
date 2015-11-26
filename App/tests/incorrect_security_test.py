54015cd5238c25f26ca82eda1e84868f2f556844
0xJCG <jonathancastrogonzalez@gmail.com>
2015-11-26 16:48:55 +0100

    Tests started.

---
+++ App/tests/incorrect_security_test.py
@@ -0,0 +1,18 @@
+__version__ = "0.1"
+__copyright__ = "CopyRight (C) 2015 by Jonathan Castro"
+__license__ = "Proprietary"
+__author__ = "Jonathan Castro"
+__author_email__ = "Jonathan Castro, jonathancastrogonzalez at gmail dot com"
+
+import unittest
+
+class TestUM(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_x(self):
+        self.assertEqual(1 == 1, True)
+
+if __name__ == '__main__':
+    unittest.main()
